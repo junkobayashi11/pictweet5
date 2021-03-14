9e92c6c857450dfe7b62c717cf43e76c31b92d84
junkobayashi11 <77084304+junkobayashi11@users.noreply.github.com>
2021-03-14 12:30:46 +0900

    feat: 検索機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
@@ -19,6 +19,10 @@ class TweetsController < ApplicationController
   end
 
   def edit
+  end
+
+  def search
+    @tweets = Tweet.search(params[:keyword])
   end
 
   def update
